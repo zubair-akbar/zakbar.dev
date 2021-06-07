23d0cf32f821c8fbd5300d58bc9b15fa832fdf52
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-06-07 14:11:53 -0400

    Boilerplate code now rendering

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [selection, setSelection] = useState('');
+  const [modal, setModal] = useState('false');
+
+  console.log('%c Woah! Made by Zubair Akbar 🤓 ', 'background: #222; color: #bada55');
+
+  return (
+    <div>Here we go</div>
+  )
+}
+
+export default App;
